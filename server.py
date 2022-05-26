afc5a6d2ffaa488b18d5c0772b89746bd6107071
Guseyn Salimov <maximweller@protonmail.com>
2022-05-26 20:25:39 +0300

    fixes

--- server.py
+++ server.py
@@ -4,31 +4,6 @@ from http import HTTPStatus
 from helper import get_open_port
 
 
-def body_fun(list_of_data, add):
-    method = list_of_data[0].split()[0]
-    try:
-        status = int(list_of_data[0].split()[1][9:])
-        phrase = HTTPStatus(status).phrase
-    except ValueError:
-        status = 200
-        phrase = HTTPStatus(status).phrase
-    headers = '\n'.join(i for i in list_of_data[2:])
-    body = f"<pre>Request method: {method}<pre>" \
-           f"<pre>Request source: {add}</pre>" \
-           f"<pre>Request status: {status} {phrase}</pre>" \
-           f"<pre>{headers}</pre>"
-    print(headers)
-    return body
-
-
-def headers_fun():
-    headers = '\r\n'.join([
-        'HTTP/1.1 200 OK',
-        'Content-Type: text/html; charset=UTF-8'
-    ])
-    return headers
-
-
 def main():
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
         srv_address = ('', get_open_port())
@@ -43,16 +18,39 @@ def main():
 
             print("\nConnection from", r_address)
             while True:
-                text = conn.recv(1024).decode('utf-8')
+                received_text = conn.recv(1024).decode('utf-8')
 
-                print(f'Received:\n\n {text} \n\n')
+                print(f'Received:\n\n {received_text} \n\n')
 
-                response = '\r\n\r\n'.join([
-                    headers_fun(),
-                    body_fun(text.split("\n"), r_address)
+                # forming body
+                list_of_received_text = received_text.split("\n")
+                method = list_of_received_text[0].split()[0]
+                try:
+                    status = int(list_of_received_text[0].split()[1][9:])
+                    phrase = HTTPStatus(status).phrase
+                except ValueError:
+                    status = 200
+                    phrase = HTTPStatus(status).phrase
+                received_headers = '\n'.join(i for i in list_of_received_text[2:])
+                body = f"<pre>Request method: {method}<pre>" \
+                       f"<pre>Request source: {r_address}</pre>" \
+                       f"<pre>Request status: {status} {phrase}</pre>" \
+                       f"<pre>{received_headers}</pre>"
+
+                # forming headers
+                headers = '\r\n'.join([
+                    f"HTTP/1.1 {status} {phrase}",
+                    "Content-Type: text/html; charset=UTF-8",
+                    f"Content-Length: {len(body.encode('utf-8'))}"
                 ])
 
-                if text:
+                # sum
+                response = '\r\n\r\n'.join([
+                    headers,
+                    body
+                ])
+
+                if received_text:
                     print("Sending data back to the client")
                     conn.send(response.encode('utf-8'))
                 else:
