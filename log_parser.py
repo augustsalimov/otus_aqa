90a6b4fac425e5768fd2187ed8ffd8ee66a38a7e
Guseyn Salimov <maximweller@protonmail.com>
2022-06-01 21:20:13 +0300

    fixes

--- log_parser.py
+++ log_parser.py
@@ -1,14 +1,3 @@
-"""
-3) Для access.log должна собираться следующая информация:
- + общее количество строк wc -l < access.log - общее количество выполненных запросов
- + количество запросов по HTTP-методам: GET - 20, POST - 10 и т.п.
- + топ 3 IP адресов, с которых были сделаны запросы
- - топ 3 самых долгих запросов, должно быть видно метод, url, ip, длительность, дату и время запроса
-
-4) Собранная статистика должна быть сохранена в json файл и выведена в терминал в следующем формате
-
-5) Должен быть README.md файл, который описывает как работает скрипт
-"""
 import argparse
 import re
 import json
@@ -17,59 +6,84 @@ from collections import defaultdict
 
 
 def parse():
+    # parsing argument from cli
     parser = argparse.ArgumentParser(description='Process access.log')
     parser.add_argument('-f', dest='file', action='store', help='Path to logfile')
     return parser.parse_args()
 
 
 def dir_to_files(my_path):
+    # return couple files in dir
     return next(os.walk(my_path), (None, None, []))[2]
 
 
 def main(args):
-    dict_ip = defaultdict(
-        lambda: {"ALL": 0, "GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0})
+    amount_of_lines = 0
+    # ip: number of uses
+    ip_dict = defaultdict(lambda: 0)
+    # methods: amount of uses
     method_dict = {"GET": 0, "POST": 0, "PUT": 0, "DELETE": 0, "HEAD": 0}
+
     longest_request = defaultdict(
         lambda: {"ip": '', "method": '', "url": '', "datetime": ''})
-    # del my_dict[sorted(my_dict)[3]]
-    with open(args.file) as lines:
-        idx = 1
-        for line in lines:
-            if idx > 9:
-                break
-            print('\n', line)
+    request = {"ip": '', "method": '', "url": '', "datetime": '', "duration": ''}
+    list_of_longest_requests = []
+    # supportive list
+    support = [0, 0, 0]
 
+    with open(args.file) as lines:
+        for line in lines:
             ip_match = re.search(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}', line)
             if ip_match is not None:
                 ip = ip_match.group()
-                dict_ip[ip]['ALL'] += 1
+                ip_dict[ip] += 1
 
                 method = re.search(r'] "(POST|GET|PUT|DELETE|HEAD)', line)
                 if method is not None:
-                    dict_ip[ip][method.group(1)] += 1
-                    method_dict[method.group(1)] += 1
+                    method = method.group(1)
+                    method_dict[method] += 1
 
                     duration = re.search(r'" \d{4,5}', line)
-                    longest_request[0]
-                    if duration is not None and duration.group() > sorted(longest_request)[0]:
-                        longest_request[duration]["ip"] = ip
-                        longest_request[duration]["method"] = method.group(1)
-                        url = re.search(r'\Bhttp://\B')
-                        print(url)
-                        longest_request[duration]["url"] = url
 
-                idx += 1
-    print(idx)
-    print('\n', method_dict, '\n')
-    print(json.dumps(dict_ip, indent=4))
+                    if duration is not None:
+                        duration = int(duration.group()[2:])
+                        if duration > support[0]:
+                            datetime = \
+                                re.search(r'\d{1,2}/\w{2,4}/\d{4}:\d{1,2}:\d{1,2}:\d{1,2} \+\d{1,4}', line).group()
+                            request["ip"] = ip
+                            request["method"] = method
+                            request["datetime"] = datetime
+                            request["duration"] = duration
+
+                            list_of_longest_requests.append(request)
+
+                            if len(list_of_longest_requests) > 3:
+                                del list_of_longest_requests[0]
+                            support[0] = duration
+                            support = sorted(support)
+                amount_of_lines += 1
+
+    # ip dict to top ip dict
+    ip_dict = {k: v for k, v in sorted(ip_dict.items(), key=lambda item: item[1])}
+    support = sorted({v: k for k, v in ip_dict.items()}, reverse=True)[:3]
+    top_ip = {k: v for k, v in ip_dict.items() if v in support}
+
+    return {"top_ips": top_ip, "top_longest": list_of_longest_requests, "total_stat": method_dict, "total_requests": amount_of_lines}
+
+
+def output(json_):
+    with open('json_file.json', 'w', encoding='utf-8') as f:
+        json.dump(json_, f, indent=4)
+    print(json.dumps(json_, indent=4))
 
 
 if __name__ == "__main__":
     result_of_parsing = parse()
+    out = ''
     if os.path.isdir(str(result_of_parsing)):
         files = dir_to_files(result_of_parsing)
         for file in files:
-            main(file)
+            out = main(file)
     else:
-        main(result_of_parsing)
+        out = main(result_of_parsing)
+    output(out)
